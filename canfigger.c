d6483b6e1ae7ca01da6d9c3ba777ad9d3569f6c2
andy5995 <andy400-dev@yahoo.com>
2021-05-30 13:12:34 -0500

    code clean-up

--- canfigger.c
+++ canfigger.c
@@ -27,8 +27,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define MAX_LINE 2048
 
-static st_canfigger_node *root;
-
 void
 canfigger_free (st_canfigger_node * node)
 {
@@ -123,18 +121,18 @@ grab_str_segment (const char *begin, const char *end, char *str)
 st_canfigger_list *
 canfigger_parse_file (const char *file, const char delimiter)
 {
-  root = NULL;
+  static st_canfigger_node *root = NULL;
   st_canfigger_list *list = NULL;
 
   FILE *fd = fopen (file, "r");
   if (fd == NULL)
     return NULL;
 
-  char line_from_config[MAX_LINE];
-  while (fgets (line_from_config, sizeof line_from_config, fd) != NULL)
+  char fd_line[MAX_LINE];
+  while (fgets (fd_line, sizeof fd_line, fd) != NULL)
   {
-    char *line_ptr = line_from_config;
-    trim_whitespace (line_ptr);
+    trim_whitespace (fd_line);
+    char *line_ptr = fd_line;
     line_ptr = del_char_shift_left (' ', line_ptr);
     switch (*line_ptr)
     {
@@ -144,67 +142,56 @@ canfigger_parse_file (const char *file, const char delimiter)
       continue;
     }
 
-    st_canfigger_node cur_node;
-    char line_copy[sizeof line_from_config];
-    strcpy (line_copy, line_ptr);
-    char *tmp_key = strchr (line_copy, '=');
-    if (tmp_key == NULL)
-    {
-      trim_whitespace (line_copy);
-      strcpy (cur_node.key, line_copy);
-      *cur_node.value = '\0';
-      *cur_node.attribute = '\0';
-    }
-    else
-    {
-      char key[MAX_LINE];
-      grab_str_segment (line_copy, tmp_key, key);
-      trim_whitespace (key);
-      strcpy (cur_node.key, key);
-      tmp_key++;
-      tmp_key = del_char_shift_left (' ', tmp_key);
-      char *tmp_value = strchr (tmp_key, delimiter);
-      if (tmp_value == NULL)
-      {
-        strcpy (cur_node.value, tmp_key);
-        trim_whitespace (tmp_key);
-        *cur_node.attribute = '\0';
-      }
-      else
-      {
-        char value[MAX_LINE];
-        grab_str_segment (tmp_key, tmp_value, value);
-        strcpy (cur_node.value, value);
-        tmp_value++;
-        tmp_value = del_char_shift_left (' ', tmp_value);
-        char *tmp_attribute = tmp_value;
-        strcpy (cur_node.attribute, tmp_attribute);
-      }
-    }
-
     st_canfigger_node *tmp_node = malloc (sizeof (struct st_canfigger_node));
     if (tmp_node != NULL)
     {
       if (root != NULL)
         list->next = tmp_node;
-      // list = push_node (tmp_node);
-      trim_whitespace (line_copy);
-      strcpy (tmp_node->key, cur_node.key);
-      strcpy (tmp_node->value, cur_node.value);
-      strcpy (tmp_node->attribute, cur_node.attribute);
+
+      char *tmp_key = strchr (line_ptr, '=');
+      if (tmp_key == NULL)
+      {
+        trim_whitespace (line_ptr);
+        strcpy (tmp_node->key, line_ptr);
+        *tmp_node->value = '\0';
+        *tmp_node->attribute = '\0';
+      }
+      else
+      {
+        char key[MAX_LINE];
+        grab_str_segment (line_ptr, tmp_key, key);
+        trim_whitespace (key);
+        strcpy (tmp_node->key, key);
+        tmp_key++;
+        tmp_key = del_char_shift_left (' ', tmp_key);
+        char *tmp_value = strchr (tmp_key, delimiter);
+        if (tmp_value == NULL)
+        {
+          strcpy (tmp_node->value, tmp_key);
+          trim_whitespace (tmp_key);
+          *tmp_node->attribute = '\0';
+        }
+        else
+        {
+          char value[MAX_LINE];
+          grab_str_segment (tmp_key, tmp_value, value);
+          strcpy (tmp_node->value, value);
+          tmp_value++;
+          tmp_value = del_char_shift_left (' ', tmp_value);
+          char *tmp_attribute = tmp_value;
+          strcpy (tmp_node->attribute, tmp_attribute);
+        }
+      }
       tmp_node->next = NULL;
       list = tmp_node;
+
       if (root == NULL)
         root = list;
-      if (list == NULL)
-      {
-        fprintf (stderr, "Error\n");
-        canfigger_free (root);
-        return NULL;
-      }
     }
     else
     {
+      if (root != NULL)
+        canfigger_free (root);
       errno = ENOMEM;
       return NULL;
     }
--- canfigger.h
+++ canfigger.h
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define CANFIGGER_VERSION LIB_VERSION
+#define CANFIGGER_VERSION "0.1.0-dev"
 
 typedef struct st_canfigger_node st_canfigger_node;
 struct st_canfigger_node

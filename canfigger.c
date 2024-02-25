de4cd0c789c4d5ce81787c2ff5e26c5750f6b68c
andy5995 <arch_stanton5995@protonmail.com>
2024-02-25 10:47:09 -0600

    use strdup wrapper function and clean up error-checking

--- canfigger.c
+++ canfigger.c
@@ -25,12 +25,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "canfigger.h"
 
-static int err_strdup = 0;
+// Function pointer type for string duplication functions
+// typedef char *(*strdup_func_t)(const char *, size_t);
+
 static int canfigger_delimiter = 0;
 char *canfigger_attr = NULL;
 
 static char *grab_str_segment(char *a, char **dest, const int c);
 static void canfigger_free(struct Canfigger **node);
+static char *strdup_generic(const char *s, size_t n,
+                            char *(*dup_func)(const char *, size_t));
 
 struct line
 {
@@ -46,6 +50,42 @@ cleanup_1(char **buf)
     free(*buf);
 
   return;
+}
+
+
+static char *
+strdup_wrapper(const char *s, size_t n)
+{
+  (void) n;                     // Unused parameter, to match function signature of strndup
+  return strdup(s);
+}
+
+
+static char *
+strdup_wrap(const char *s)
+{
+  return strdup_generic(s, 0, strdup_wrapper);  // Use the wrapper function for strdup
+}
+
+
+static char *
+strndup_wrap(const char *s, size_t n)
+{
+  return strdup_generic(s, n, strndup); // Directly use strndup
+}
+
+
+static char *
+strdup_generic(const char *s, size_t n,
+               char *(*dup_func)(const char *, size_t))
+{
+  char *retval = dup_func(s, n);
+
+  if (retval)
+    return retval;
+
+  fprintf(stderr, "Failed to duplicate string: %s\n", strerror(errno));
+  return NULL;
 }
 
 
@@ -86,6 +126,7 @@ init_first_attr(struct attributes *attributes)
   canfigger_free_current_attr_str_advance(attributes);
   return;
 }
+
 // that probably exists to help with code clarity. It will copy the first
 // attribute in a 'attr1, attr2, attr3,...' string to attributes->current and
 // point 'canfigger_attr' to that address.
@@ -213,19 +254,14 @@ grab_str_segment(char *a, char **dest, const int c)
   char *b = strchr(a, c);
   if (!b)
   {
-    *dest = strdup(a);
-    if (!*dest)
-      err_strdup = 1;
+    *dest = strdup_wrap(a);
     return b;                   // NULL
   }
 
-  *dest = strndup(a, b - a);
+  *dest = strndup_wrap(a, b - a);
   if (!*dest)
-  {
-    if (!*dest)
-      err_strdup = 1;
     return NULL;
-  }
+
   trim_whitespace(*dest);
   return b + 1;
 }
@@ -297,8 +333,6 @@ read_entire_file(const char *filename)
 struct Canfigger *
 canfigger_parse_file(const char *file, const int delimiter)
 {
-  err_strdup = 0;
-
   struct Canfigger *root = NULL, *cur_node = NULL;
   struct line line;
 
@@ -325,35 +359,23 @@ canfigger_parse_file(const char *file, const int delimiter)
 
     int r = add_key_node(&root, &cur_node);
     if (r == -1)
-    {
-      canfigger_free(&root);
-      cleanup_1(&file_contents);
-      return NULL;
-    }
+      break;
 
     // Get key
     cur_node->key = NULL;
     char *b = grab_str_segment(a, &cur_node->key, '=');
-
-    if (err_strdup)
-    {
-      cleanup_1(&file_contents);
-      return NULL;
-    }
+    if (errno)
+      break;
 
     // Get value
     cur_node->value = NULL;
 
     if (b)
     {
-      a = b;
+      a = b;                    // TODO fix this
       b = grab_str_segment(a, &cur_node->value, delimiter);
-    }
-
-    if (err_strdup)
-    {
-      cleanup_1(&file_contents);
-      return NULL;
+      if (errno)
+        break;
     }
 
     // Handle attributes
@@ -369,11 +391,11 @@ canfigger_parse_file(const char *file, const int delimiter)
 
       struct attributes *attr_ptr = cur_node->attributes;
       attr_ptr->current = NULL;
-      attr_ptr->str = strdup(b);
-      if (!attr_ptr->str)
-      {
-      }
-      // Handle error
+
+      attr_ptr->str = strdup_wrap(b);
+      if (errno)
+        break;
+
       attr_ptr->ptr = attr_ptr->str;
     }
     else
@@ -383,15 +405,22 @@ canfigger_parse_file(const char *file, const int delimiter)
     free_cur_line_and_advance(&line);
   }
 
-  if (!root)
+  if (!root || errno)
   {
+    if (root)
+      canfigger_free(&root);
     cleanup_1(&file_contents);
-    free(line.line);
+    if (line.line)
+      free(line.line);
     return NULL;
   }
 
   cleanup_1(&file_contents);
-  free(line.line);
+  if (line.line)
+    free(line.line);
+
+  if (!root)
+    return NULL;
 
   canfigger_delimiter = delimiter;
   init_first_attr(root->attributes);

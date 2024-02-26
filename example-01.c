30a8353260cfae05869cb0c349d4034d75129e44
andy5995 <arch_stanton5995@protonmail.com>
2024-02-25 18:41:55 -0600

    example-01.c: add more comments

--- example-01.c
+++ example-01.c
@@ -5,16 +5,35 @@
 int
 main(void)
 {
+  //
+  // Get a linked list containing the parsed config file. Each node contains
+  // a key (or a "setting", or an "option"), a value and attributes (if they
+  // are provided in your program's configuration file.
+  //
   struct Canfigger *config = canfigger_parse_file("../example-01.conf", ',');
   while (config != NULL)
   {
+    //
+    // The value member of the node must be checked for NULL
+    // before using it.
+    //
     printf("Key: %s, Value: %s\n", config->key,
            config->value != NULL ? config->value : "NULL");
 
+    //
     // Process attributes if necessary
+    //
+    // You must check canfigger_attr for NULL before doing an operation on it,
+    // such as strcmp or printf. If it is not NULL, it points to the the first
+    // attribute (or the *only* attribute if only one was used
+    //
     while (canfigger_attr)
     {
       printf("Attribute: %s\n", canfigger_attr);
+
+      //
+      // Get the next attribute in the list (if there is one).
+      //
       canfigger_free_current_attr_str_advance(config->attributes);
     }
 

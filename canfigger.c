863bd4bf64e2a10a63362180b478c049dbdf22b2
andy5995 <arch_stanton5995@protonmail.com>
2024-02-26 02:55:31 -0600

    Tweak license header (remove "at your option")

--- canfigger.c
+++ canfigger.c
@@ -6,7 +6,7 @@ Copyright (C) 2021-2024 Andy Alt (arch_stanton5995@proton.me)
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of

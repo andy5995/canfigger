07bc1f7d05e42aec251075dfbd88ed8151388bdd
andy5995 <arch_stanton5995@protonmail.com>
2024-02-27 11:39:16 -0600

    docs: Regenerate

--- docs/menudata.js
+++ docs/menudata.js
@@ -27,7 +27,7 @@ var menudata={children:[
 {text:"README",url:"index.html"},
 {text:"Structures",url:"annotated.html"},
 {text:"File List",url:"files.html"},
-{text:"File Members",url:"globals.html",children:[
+{text:"Globals",url:"globals.html",children:[
 {text:"All",url:"globals.html"},
 {text:"Functions",url:"globals_func.html"}]},
 {text:"Examples",url:"examples.html"}]}

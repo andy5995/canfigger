47789718d19517b1eadd357336f2634286071b77
andy5995 <arch_stanton5995@protonmail.com>
2024-02-28 18:06:18 -0600

    Regenerate docs

--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -3,7 +3,7 @@ var searchData=
   ['canfigger_0',['Canfigger',['../canfigger_8h.html#structCanfigger',1,'']]],
   ['canfigger_1',['canfigger',['../index.html',1,'']]],
   ['canfigger_2',['Projects using canfigger',['../index.html#autotoc_md6',1,'']]],
-  ['canfigger_20v0_203_200999_3',['canfigger v0.3.0999',['../index.html#autotoc_md0',1,'']]],
+  ['canfigger_20v0_203_200_3',['canfigger v0.3.0',['../index.html#autotoc_md0',1,'']]],
   ['canfigger_2eh_4',['canfigger.h',['../canfigger_8h.html',1,'']]],
   ['canfigger_5ffree_5fcurrent_5fattr_5fstr_5fadvance_5',['canfigger_free_current_attr_str_advance',['../canfigger_8h.html#ae6d70f9e366beaa126a989bb2b4457b8',1,'canfigger.c']]],
   ['canfigger_5ffree_5fcurrent_5fkey_5fnode_5fadvance_6',['canfigger_free_current_key_node_advance',['../canfigger_8h.html#a095f7a89a1f2e3422870165280205a0b',1,'canfigger.c']]],
--- docs/search/all_b.js
+++ docs/search/all_b.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['v0_203_200999_0',['canfigger v0.3.0999',['../index.html#autotoc_md0',1,'']]]
+  ['v0_203_200_0',['canfigger v0.3.0',['../index.html#autotoc_md0',1,'']]]
 ];
